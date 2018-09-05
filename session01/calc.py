af18764a683f944a4d1c5fdbf66fe5dfddb5c805
Connie <cli4@babson.edu>
2018-09-05 14:06:43 -0400

    Revert "Class 2: Exercise 1 (9/4)"
    This reverts commit e0c281a8e87aaf285ad36a56ffc93ef68b7b3bff.

--- session01/calc.py
+++ session01/calc.py
@@ -11,37 +11,3 @@ print (412.482/60)
 
 #avg pace mile/sec
 print (2562/6.211180124223602)
-
-# 9/4/2018 Exercise 1 
-# What is the volume of a sphere with radius 5?
-pi = 3.14
-r_cubed = 5 * 5 * 5
-vol = 4 / 3 
-
-print('The volume of a sphere with a radius of 5 is {}'.format(vol * pi * r_cubed))
-
-#Suppose the cover price of a book is $24.95, but bookstores get a 40\% discount. Shipping costs $3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for 60 copies?
-initial_ship = 3
-additional_ship = (3/4)
-price = 24.95
-discount = (6/10)
-
-print('The total wholesale cost for 60 copies of the books is {}'.format(price * discount * 60 + initial_ship * 1 + additional_ship * 59))
-
-#If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
-
-start = 6 + 52 / 60.0
-easy = (8 + 15 / 60.0 ) / 60.0
-fast= (7 + 12 / 60.0) / 60.0
-total_run = 2 * easy + 3 * fast
-bfast_hr = start + total_run
-bfast_min = (bfast_hr-int(bfast_hr))*60
-bfast_sec= (bfast_min-int(bfast_min))*60
-
-print ('You will finish and have breakfast by', int(bfast_hr),':', int (bfast_min),':', int (bfast_sec) )
-
-#If my average grade rises from 82 to 89. What is the percentage of the increase? Format the result as 'xx.x%'. Keep one figure after decimal point.
-
-perc_inc = ((((89 - 82) / 82) * 100))
-
-print('The percentage increase is {:.2f}%'.format(perc_inc))
--- session01/hello.py
+++ session01/hello.py
@@ -1,7 +1,1 @@
-first_name = input('Please enter your first name: ')
-
-last_name = input('Please enter your last name: ')
-
-name = first_name + ' ' + last_name
-
-print('Hello,',name)
+print('Hello, Connie!')
