e0c281a8e87aaf285ad36a56ffc93ef68b7b3bff
Connie <cli4@babson.edu>
2018-09-05 14:05:17 -0400

    Class 2: Exercise 1 (9/4)

--- session01/hello.py
+++ session01/hello.py
@@ -1,1 +1,7 @@
-print('Hello, Connie!')
+first_name = input('Please enter your first name: ')
+
+last_name = input('Please enter your last name: ')
+
+name = first_name + ' ' + last_name
+
+print('Hello,',name)
